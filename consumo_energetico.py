54e9a8fd786882d9aea6b4336eedef0f216fda25
Naza2006 <nazario@uamv.edu.ni>
2025-05-15 21:08:00 -0600

    Inclui los dias de la semana

--- consumo_energetico.py
+++ consumo_energetico.py
@@ -5,4 +5,6 @@ turnos_dia = 3
 
 for edificio in range(num_edificios):
     print(f"\nedificio {edificio + 1}:") #Inicio del bucle
-
+    for dia in range(dias_semana):
+        print(f" dia {dia + 1}:")
+        for turno in range(turnos_dia): #definir los 7 dias de la semana
