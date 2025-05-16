e7ab38d1fb8e88e72d37dfa4f3228c24523ecfbd
Naza2006 <nazario@uamv.edu.ni>
2025-05-15 21:15:01 -0600

    Buvle de los 3 turnos

--- consumo_energetico.py
+++ consumo_energetico.py
@@ -7,4 +7,13 @@ for edificio in range(num_edificios):
     print(f"\nedificio {edificio + 1}:") #Inicio del bucle
     for dia in range(dias_semana):
         print(f" dia {dia + 1}:")
-        for turno in range(turnos_dia): #definir los 7 dias de la semana
+        for turno in range(turnos_dia):
+            while True:
+                try:
+                    consumo = float(input(f" Ingrese el comsumo en KWH {turno + 1}:"))
+                    break
+                except ValueError:
+                    print(" Error, ingrese un numero valido")
+            consumo_total[efificio] += consumo  # Bucle de los 3 trunos del dias
+
+print()
