2c461ab1e6d1fe14b566cba84b0e940d2225d3ba
andreadelaroca <asodelaroca@uamv.edu.ni>
2025-05-15 21:12:26 -0600

    Añadí un bucle interno for

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,21 +1,22 @@
 # monitoreo del consumo energético
 # registrar el consumo energético de cuatro edificios del campus a lo largo de una semana.
-# por cada día se ingresarán los kilovatios (kW) consumidos en tres turnos: mañana, 
-# tarde y noche. 
-# el programa debe calcular el consumo total por edificio y el promedio semanal.
+# por cada día se ingresarán los kilovatios (kW) consumidos en tres turnos.
+# calcular el consumo total por edificio y el promedio semanal.
 
 # definición de variables iniciales
 consumo_total = 0 # variable para almacenar el consumo total de kW
 consumo_semanal = 0 # variable para almacenar el consumo semanal de kW
 consumo_diario = 0 # variable para almacenar el consumo diario de kW
 consumo_turno = 0 # variable para almacenar el consumo por turno
+dias = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"] # lista de días
 turnos = ["Mañana", "Tarde", "Noche"] # lista de turnos
 
 print("Monitoreo del consumo energético de cuatro edificios en UAM a lo largo de una semana")
 
-#bucle para cada edificio
+# bucle para cada edificio
 for edificio in range(1,6):
-    print(f"\nEdificio No.{edificio}") #imprime el edificio que se está procesando
-    
-    
-    
+    print(f"\nEdificio No. {edificio}") # imprime el edificio que se está procesando
+    # bucle para cada día de la semana
+    for dia in dias:
+        print(f"Día {dia}")
+        
